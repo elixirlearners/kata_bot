a71e939c6b7530c913ea48a7c02d1582bbd39e31
Caleb Gasser <1561693+calebgasser@users.noreply.github.com>
2023-10-28 16:26:01 -0500

    docker file changes

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule KataBot.MixProject do
       {:ecto_sql, "~> 3.10"},
       {:postgrex, "~> 0.17.3"},
       {:pots, "~> 0.1.1"},
+      # {:pots, path: "/home/cgasser/code/pots", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
